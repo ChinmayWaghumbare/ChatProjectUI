dcd94c35e8cae82285a79835d2d1650977d6a4c2
Chinmay <cwaghumbar@gmail.com>
2018-12-06 14:59:04 +0530

    First Commit

---
+++ WebClient3/js/login.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+    $('#logIn').click(function () {
+        var uid = $('#userName').val();
+        var pwd = $('#password').val();
+
+        var param = {
+                ID: 0,
+                UID: "Chinmay",
+                UPWD: "1234"
+        };
+    
+
+        $.ajax({
+            url: 'http://localhost:64002/api/LOGINs/Login',
+            type: 'POST',
+            processData:false,
+            contentType:'application/json',
+            dataType: 'plain/text',
+            data: JSON.stringify(param),
+            crossDomain:true,
+            success: function (result) {
+                alert(result);
+            },
+            error: function (result) {
+                alert(result);
+            }
+
+        });
+    });
+
+
+    
+
+});
---
+++ WebClient3/js/signUp.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    $('#signUp').click(function () {
+        var uid = $('#userName').val();
+        var pwd = $('#password').val();
+
+        var param = {
+            ID: 0,
+            UID: "Chinmay",
+            UPWD: "1234"
+        };
+
+
+        $.ajax({
+            url: 'http://localhost:64002/api/LOGINs/AddNewUser',
+            type: 'POST',
+            processData: false,
+            contentType: 'application/json',
+            dataType: 'plain/text',
+            data: JSON.stringify(param),
+            crossDomain: true,
+            success: function (result) {
+                alert(result);
+            },
+            error: function (result) {
+                alert(result);
+            }
+
+        });
+    });
+
+
+    
+});
