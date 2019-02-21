780a854541f0f50ddd612219e566e4c0ea2229b7
Chinmay <cwaghumbar@gmail.com>
2019-02-22 00:34:36 +0530

    Error solves

--- WebClient3/js/chatWindow.js
+++ WebClient3/js/chatWindow.js
@@ -2,6 +2,27 @@
 
     $('#backBtn').click(function () {
         $(location).attr('href', 'messageList.html');
-    })
+    });
+
+    var fromUserName = sessionStorage.getItem('fromUserName');
+    var toUserName = sessionStorage.getItem('userName');
+
+    //var data = { fromUser: fromUserName, toUser: toUserName };
+    //data = JSON.stringify(data);
+    $.ajax({
+        type:'GET',
+        url: 'http://localhost:64002/api/MESSAGEMAST/getMessages?fromUser='+fromUserName+'&toUser='+toUserName,
+        contentType: 'application/json',
+        dataType:'text',
+        //data: data,
+        crossDomain:true,
+        success: function (data) {
+
+        },
+        error: function (data) {
+
+        }
+    });
+
 
 });
--- WebClient3/js/messageList.js
+++ WebClient3/js/messageList.js
@@ -3,15 +3,17 @@
 
     var userName = sessionStorage.getItem('userName');
     $.ajax({
-        url: 'http://localhost:64002/api/MESSAGEMAST/getMessagesList?userName=' + userName,
+        url: 'http://localhost:64002/api/MESSAGEMAST/getMessageList?userName=' + userName,
         type: 'GET',
         dataType: 'json',
         contentType: 'text',
         crossDomain: true,
         success: function (data) {
-            length = data.Length;
+            //length = data.Length;
             //data = jQuery.parseJSON(data);
-            for (var i = 0; i < data.Length; i++) {
+            for (var i = 0; i < data.length; i++) {
+
+                $('#messageList').append("<div id='rowMsg' data='"+data[i].name+"' class='shadow p-3 mb-2 bg-white rounded col-12'><div class='col-9'>" + data[i].name + "</div><div class='col-3'>" + data[i].count + "</div></div>");
 
             }
         },
@@ -19,4 +21,20 @@
 
         }
     });
+
+
+    $(document).on('click', '#rowMsg', function () {
+        user = $(this);
+
+        console.log(user[0].attributes.data.value);
+        user=user[0].attributes.data.value;
+        getMessage(user);
+    });
+    var getMessage = function (fromUserName) {
+
+        sessionStorage.setItem('fromUserName', fromUserName);
+        $(location).attr('href', 'ChatWindow.html');
+    };
+
 });
+
